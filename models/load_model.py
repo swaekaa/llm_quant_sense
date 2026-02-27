1d4afeb0e2db34f11075b6f437f2b386b2375397
swaekaa <sawariaekaansh@gmail.com>
2026-02-27 21:09:04 +0530

    start analysis of phi-2

--- models/load_model.py
+++ models/load_model.py
@@ -1,19 +1,26 @@
 import torch
-from transformers import AutoModelForSequenceClassification
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 
-def load_model(model_name, device="cpu"):
-    """
-    Loads pretrained LLM and extracts transformer layers.
-    """
+def load_model(model_name="microsoft/phi-2"):
 
-    model = AutoModelForSequenceClassification.from_pretrained(model_name)
-    model.to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    quant_config = BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4"
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=quant_config,
+        device_map="auto"
+    )
+
     model.eval()
 
-    transformer_layers = None
+    transformer_layers = model.model.layers
 
-    if "distilbert" in model_name.lower():
-        transformer_layers = model.distilbert.transformer.layer
-
-    return model, transformer_layers
+    return model, tokenizer, transformer_layers
